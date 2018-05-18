0a0c58b2a2544a6adf0381e022457f025bb38e82
Firas Rafislam <firasrafislam@live.com>
2018-05-18 11:01:51 +0700

    Tasting A New GuestRoom Picker

---
+++ TiketReactions/ViewControllers/HotelsTab/HotelDiscovery/Filters/HotelDiscoveryFiltersDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  HotelDiscoveryFiltersDataSource.swift
+//  TiketReactions
+//
+//  Created by Firas Rafislam on 14/05/18.
+//  Copyright © 2018 Firas Rafislam. All rights reserved.
+//
+
+import Foundation
