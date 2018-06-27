379c62a1786b917f77e2c68e1eaecf9ec44be7cc
Firas Rafislam <firasrafislam@live.com>
2018-06-27 18:22:35 +0700

    Shaping OrderList With NavPayments

---
+++ TiketReactions/Styles/DelightStepper.swift
@@ -0,0 +1,21 @@
+//
+//  DelightStepper.swift
+//  TiketReactions
+//
+//  Created by Firas Rafislam on 27/06/18.
+//  Copyright © 2018 Firas Rafislam. All rights reserved.
+//
+
+import UIKit
+
+class DelightStepper: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
